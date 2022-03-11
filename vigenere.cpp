05450361ea4268d3245eb39297508f04db16dd4c
Henry Fan <henry.fan2313@gmail.com>
2022-03-11 01:35:28 -0500

    commited parts of lab

--- vigenere.cpp
+++ vigenere.cpp
@@ -2,8 +2,25 @@
 #include <string>
 
 char shiftChar(char c, int rshift) {
-  char encodedchar = c + rshift;
-  return encodedchar;
+  char caps[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+  char lowercase[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+  if (cipherascii > 96 && cipherascii < 123) {
+    int charindex = int(c) - 97;
+    charindex += rshift;
+    int encodedindex = charindex % 26;
+    char encodedchar = lowercase[encodedindex];
+    return encodedchar;
+  }
+  else if ((cipherascii > 64 && cipherascii < 91)) {
+    int charindex = int(c) - 65;
+    charindex += rshift;
+    int encodedindex = charindex % 26;
+    char encodedchar = caps[encodedindex];
+    return encodedchar;
+  }
+  else {
+    return c;
+  }
 }
 
 std::string encryptVigenere(std::string plaintext, std::string keyword) {
